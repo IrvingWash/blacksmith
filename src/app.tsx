3f9556baa5de46b131934f360b6c85de806f401a
Washington Irving <iamwashingyou@gmail.com>
2023-09-24 12:32:27 +0300

    mark up header

--- src/app.tsx
+++ src/app.tsx
@@ -1,31 +1,13 @@
-import React, { useEffect } from 'react';
-import { LastFM } from './last-fm-api/last-fm';
-import { RecentTrack } from '@domain/entities';
+import React from 'react';
+
+import { Header } from './gui/header/header';
 
 export function App(): JSX.Element {
-	const lastFM = new LastFM();
-
-	useEffect(() => {
-		const token = lastFM.authorizationProvider.getAuthenticationToken();
-
-		if (token !== null) {
-			lastFM.authorizationProvider.authorize(token);
-		}
-	}, []);
-
 	return (
-		<main>
-			<button
-				onClick={ (): void => lastFM.authorizationProvider.requestAccess('http://localhost:8080') }
-			>
-				Grant
-			</button>
-
-			<button
-				onClick={ (): Promise<void | RecentTrack[]> => lastFM.getRecentTracks({ username: lastFM.getUsername() ?? '' }).then((tracks) => console.log(tracks)) }
-			>
-				Recent tracks
-			</button>
-		</main>
+		<>
+			<Header />
+			<main>
+			</main>
+		</>
 	);
 }
